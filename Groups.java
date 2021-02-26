d2fb82c5ad9bebc93e25b83cd3214bbed0882c49
renfei <renfei@cfit.cn>
2021-02-26 16:04:48 +0800

    small modify Groups.java

--- Groups.java
+++ Groups.java
@@ -4,8 +4,9 @@ public abstract class   Groups {
     private Interger groupno;
     private String   name;
     
+    // 设置名称
     public void setName(String name){
-        this.name=name;
+        this.name = name;
     }
 
 }
