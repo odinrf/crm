a3c8fc0e3f5ae4c187d73d7b5017ecd695f4136c
renfei <renfei@cfit.cn>
2021-02-26 11:16:07 +0800

    modify GC.java add getName,getNo,setNo meth

--- GC.java
+++ GC.java
@@ -1,7 +1,19 @@
 
 
 public abstract class   GC {
-    private Interger gcno;
-    private Interger gcname;
-    private Interger groupno;
+    private Interger gcNo;
+    private String gcName;
+    
+    public Interger getNo(){
+        return this.gcNo;
+    }
+    
+    public String getName(){
+        return this.gcName;
+    }
+
+    public void setNo(Interger gcNo){
+        this.gcNo = gcNo;
+    }
+
 }
